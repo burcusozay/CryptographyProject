a4df4d1f67af5a62674f6b1beaef6dab3bc21055
burcusozay <burcusozay@gmail.com>
2013-07-09 12:57:32 +0300

    A RSA project that doesn not contain CSharp RSACryptoServiceProvider

---
+++ RSAProjesi/RSAProjesi/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace RSAProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox_RSA_PublicKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(23, 87);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(458, 145);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(23, 286);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(458, 145);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(406, 437);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(406, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Create Key";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox_RSA_PublicKey
+            // 
+            this.textBox_RSA_PublicKey.Location = new System.Drawing.Point(87, 26);
+            this.textBox_RSA_PublicKey.Name = "textBox_RSA_PublicKey";
+            this.textBox_RSA_PublicKey.ReadOnly = true;
+            this.textBox_RSA_PublicKey.Size = new System.Drawing.Size(313, 20);
+            this.textBox_RSA_PublicKey.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Public Key:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(504, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(76, 433);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(587, 24);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(76, 433);
+            this.listBox2.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 64);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Enter Text";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 483);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_RSA_PublicKey);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox_RSA_PublicKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ RSAProjesi/RSAProjesi/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.IO;
+
+
+namespace RSAProjesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DialogResult result;
+        string path;
+        char[] dizi;
+        private BigInteger a, b, phi, n=null;
+        long p, q, sifreliDeger;
+        string sifreliDizi;
+        BigInteger[] sifreliDegerDizisi;
+        int exp;
+        public Dictionary<char, int> TurkceAlfabe;
+        private BigInteger AralarindaAsalBul(BigInteger phi, BigInteger b)
+        {
+            BigInteger i = b - 1;
+
+            while (i < b || i > 1)
+            {
+                if (phi % i != 0)
+                {
+                    goto etiket;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+        etiket:
+            return i;
+        }
+        private bool AralarindaAsalMi(BigInteger phi, BigInteger b)
+        {
+            BigInteger i = 2;
+            while (i < b)
+            {
+                if (phi % i == 0 && b % i == 0)//ikisi birden bölünüyorsa
+                    return false;
+                else
+                    i++;
+            }
+            return true;
+        }
+        private bool Asalmi(BigInteger sayi)
+        {
+            if (sayi == 2) return true;
+            if (sayi < 2 || sayi % 2 == 0) return false;
+            for (long i = 3; i <= (Math.Sqrt(sayi.LongValue())); i += 2)
+                if (sayi % i == 0)
+                    return false;
+            return true;
+        }
+        public void CreateKeys() 
+        {
+            Random rnd = new Random();
+            while (true)
+            {
+            etiket1:
+                p = rnd.Next(100, 999); //ilk sayı üretimi
+                if (Asalmi(p)) //eğer sayı asal ise
+                {
+                etiket2:
+                    q = rnd.Next(100, 999);//ikinci sayı üretimi
+                    if (Asalmi(q))                //ilk sayı asalken ikinci sayı da asalsa    
+                        break;                    //döngüden çık
+                    else
+                        goto etiket2;   //ikinci sayı asal değilse yeniden q üret
+                }
+                else
+                    goto etiket1;                //ilk sayı asal değilse yeniden p üret
+            }
+
+            n = p * q; // n bulunur
+            phi = EulerAlgorithm(p, q);//phi(n) sonucunu döndürür
+            b = BigInteger.genPseudoPrime(12, 2, rnd); //rnd.Next(1, phi.IntValue());
+            while (!AralarindaAsalMi(phi, b))
+            {
+                b = AralarindaAsalBul(phi, b);
+            }
+            //burada int e döndürülmemesi gerekiyor. long kabul eden bulunmalı
+            a = b.modInverse(phi);// ModulerTersAl(b, phi);// gizli anahtar b^-1 = a bulunur          
+            textBox_RSA_PublicKey.Text = b.ToString();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox_RSA_PublicKey.ReadOnly = true;
+            CreateKeys();            
+        }
+        private long EulerAlgorithm(long p, long q)//Big int döndürmeli
+        {
+            long euler = (p - 1) * (q - 1);
+            return euler;
+        }
+        public string OpenFile()
+        {
+            string FilePath = "";
+            openFileDialog1.Title = "Lütfen Dosya Seçiniz";
+            // openFileDialog1.Filter = "Text files(*.txt)|*.txt";
+            openFileDialog1.Filter = " (*.txt)|*.txt";//|(*.docx)|*.docx|(*.doc)|*.doc";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog1.Multiselect = false;
+            result = openFileDialog1.ShowDialog();
+            path = openFileDialog1.FileName;
+            foreach (string str in openFileDialog1.FileNames)
+                FilePath = str;
+            return FilePath;
+        }
+        public void TextYaz(RichTextBox cipher, RichTextBox plain, ListBox frekans, ListBox solution)
+        {
+            if (checkBox1.Checked == false)
+            {
+                cipher.Text = "";
+                frekans.Items.Clear();
+                solution.Items.Clear();
+                OpenFile();
+                if (result == DialogResult.OK)
+                {
+                    StreamReader FileYKCipherText = new StreamReader(File.OpenRead(path));
+                    DataTable dt = new DataTable();
+                    string first_row = FileYKCipherText.ReadLine();
+                    while (first_row != null && !FileYKCipherText.EndOfStream)
+                    {
+                        cipher.Text += first_row;
+                        first_row = FileYKCipherText.ReadLine();
+                    }
+                    cipher.Text += " " + first_row;
+                    FileYKCipherText.Close();
+                }
+                else
+                    MessageBox.Show("Lütfen bir dosya seçiniz!");
+
+            }
+            
+            plain.Text = "";
+        }
+        private void FrekansHesapla(RichTextBox metin)
+        {
+            dizi = metin.Text.ToCharArray();
+        }
+
+        private BigInteger KareAlmaVeCarpma(BigInteger deger, BigInteger b, BigInteger n)
+        {
+            string binValue = Convert.ToString(b.LongValue(), 2);
+            //BigInteger intValue = Convert.ToInt32(binValue, 2);
+            int l = binValue.Length;
+            BigInteger sonuc = 1;
+            //Array.Reverse(binValue.ToArray(), 0, (l - 1));
+            for (int i = 0; i < l; i++)
+            {
+                if (binValue[i] == '1')
+                {
+                    sonuc = (sonuc * sonuc * deger) % n;
+                }
+                else
+                {
+                    sonuc = (sonuc * sonuc) % n;
+                }
+            }
+            return sonuc;
+        }
+        private long NGraphDonusumu(int[] deger, int grafsayisi, int alfabeBoyutu, BigInteger n) 
+        {            
+            long sifreliDeger=0;
+            int temp = grafsayisi;
+            for (int i = 0; i < grafsayisi; i++)
+            {
+                sifreliDeger += (int)( Math.Pow(alfabeBoyutu, temp - 1))*deger[i];              
+                temp--;
+            }       
+            return sifreliDeger;
+        }
+        public int KacGrafDonusumuYapilacak(BigInteger n, int alfabeBoyutu) 
+        {
+            int graphsayisi = 0, graf_deger=1;
+            while ( graf_deger-1<n ) 
+            {
+                graf_deger *= alfabeBoyutu;
+                graphsayisi++;
+            }
+            return graphsayisi-1;
+        }
+        private string KarakterSifreleme(string ascii, BigInteger b, BigInteger n, BigInteger phi)
+        {
+            //BigInteger[] asciiDeger= new BigInteger[ascii.Length];
+            string sifreliKarakter = "", temp = "";
+            //ascii = ascii.Replace(" ", ""); 
+            char[] charDizisi = ascii.ToLower().ToCharArray();
+            
+            int substringindex = 0;
+            int graf_sayisi=KacGrafDonusumuYapilacak(n,TurkceAlfabe.Count);
+            string donusumyap;
+            int[] asciiDeger = new int[graf_sayisi];//bir graf dönüşümü için o kadar harfin değerini tutar
+            if (charDizisi.Length%graf_sayisi!=0)
+            {                
+                for (int i = 0; i < graf_sayisi-(charDizisi.Length%graf_sayisi); i++)
+                {
+                    temp += " ";//eksik kalan yerler boşluk ile doldurulur
+                }
+            }
+            ascii += temp;
+            charDizisi = ascii.ToCharArray();
+            sifreliDegerDizisi = new BigInteger[ascii.Length / graf_sayisi];
+
+            for (int i = 0; i < ascii.Length/graf_sayisi; i++)
+            {
+                donusumyap = ascii.Substring(substringindex,graf_sayisi);//ilk n karakteri alır
+                int sayac = 0;
+                for (int j = substringindex; j < graf_sayisi + substringindex; j++)
+                {
+                    var keys = from entry in TurkceAlfabe
+                               where entry.Key == charDizisi[j]
+                               select entry.Value;
+                    foreach (var key in keys)
+                        if (TurkceAlfabe.ContainsKey(charDizisi[j]))
+                        {
+                            asciiDeger[sayac] = key;
+                            sayac++;
+                        }
+                }               
+                sifreliDeger = NGraphDonusumu(asciiDeger, graf_sayisi, TurkceAlfabe.Count, n);//Donusum degeri hesaplanır           
+                sifreliDegerDizisi[i] = KareAlmaVeCarpma(sifreliDeger, b, n);
+                sifreliKarakter += GrafDonusumu(sifreliDegerDizisi[i], n, TurkceAlfabe.Count);//ASCII karakter tablosu için 256
+                
+                substringindex += graf_sayisi;
+            }
+            return sifreliKarakter;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox2.Text = "";
+                TextYaz(richTextBox1, richTextBox2, listBox1, listBox2);
+                sifreliDizi = KarakterSifreleme(richTextBox1.Text, b, n, phi);//şifrelenmiş karakterler atanır.
+                richTextBox2.Text = sifreliDizi.ToString();
+            }
+            catch (NullReferenceException NRE)
+            {
+                MessageBox.Show("Please create a public key to encryption");
+            }
+            finally 
+            {
+                
+            }
+           
+        }
+        private string GrafDonusumu(BigInteger deger, BigInteger n, BigInteger karakterMod) //n şifreleme yapılan mod, karaktermod ise donusum için kullanılacak alfabe
+        {
+            BigInteger gecici = 1, k;
+            string sifreliKarakter = "";
+           
+           exp = 0; // exp üs derecesini tutuyor, k ise katsayı
+            while (n>gecici)
+            {
+                exp++;
+                gecici *= karakterMod;
+                //kaç graf dönüşümü yapılacağını tutar
+            }
+            exp--;
+            gecici /= karakterMod;
+            //fazladan çarpılan bölünüyor.
+            for (int j = 0; j < exp; j++)
+            {
+                {
+                    k = deger / gecici;
+                    deger -= k * gecici;
+                   
+
+                    var keys = from entry in TurkceAlfabe
+                               where entry.Value == k.IntValue()
+                               select entry.Key;
+                    foreach (var key in keys)
+                        sifreliKarakter+= key;
+
+                      //System.Text.ASCIIEncoding.UTF32.GetChars(new byte[] { 2 }); ; //System.Text.Encoding.ASCII.GetChars(new byte[] { 2 });           
+                    gecici /= karakterMod;
+                }
+            }
+            //sifreliKarakter += (char)deger.IntValue();
+            return sifreliKarakter;
+        }
+        public string SifreCoz(string sifreliMetin) 
+        {
+            BigInteger[] plaintext=new BigInteger[sifreliDegerDizisi.Length];
+            char[] cipher=sifreliMetin.ToCharArray();  
+            char [] CozulenHarfDegeri=new char[exp];
+            string cozulenMetin = "";
+            for (int i = 0; i < sifreliDegerDizisi.Length; i++)
+			{
+                plaintext[i]= KareAlmaVeCarpma(sifreliDegerDizisi[i],a, n);//buradan gelen değer sonucunda harflerin çözülmesi için graf dönüşümü yapılır
+                BigInteger temp = plaintext[i];
+                BigInteger ust, katsayi;
+                int grap_sayisi=exp;
+                for (int j = 0; j<exp; j++)
+                {
+                   ust = (long)(Math.Pow(TurkceAlfabe.Count,grap_sayisi-1));
+                   katsayi = temp / ust;
+                   temp -= ust * katsayi; 
+                    
+                    var keys = from entry in TurkceAlfabe
+                               where entry.Value == katsayi
+                               select entry.Key;
+                    foreach (var key in keys)
+                        CozulenHarfDegeri[j] = key;
+                    cozulenMetin += CozulenHarfDegeri[j];
+                    grap_sayisi--;
+                }
+                
+			}
+           
+            return cozulenMetin;
+           
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+           //richTextBox1.Text = "Encrypted Text is: "+SifreCoz(sifreliDizi);
+           MessageBox.Show("Encrypted Text: " +SifreCoz(sifreliDizi));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TurkceAlfabe = new Dictionary<char, int>();
+            char[] trk_alf={'a','b','c','ç','d','e','f','g','ğ','h','ı','i','j','k','l','m','n','o','ö','p','r','s','ş','t','u','ü','v','y','z',' ','.','*','+','-','_','/',',','%','½','&','\\','?','!','\'','#','<','>','|','^','{','}','(',')','[',']','=','0','1','2','3','4','5','6','7','8','9','"','@','q','w','x','~',';',':','é','$','€','æ','₺','ß','£','A','B','C','Ç','D','E','F','G','Ğ','H','I','İ','J','K','L','M','N','O','Ö','P','R','S','Ş','T','U','Ü','V','Y','Z'};
+            for (int i = 0; i < trk_alf.Length; i++)
+            {
+                TurkceAlfabe.Add(trk_alf[i],i);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked==true)
+            {
+                richTextBox1.ReadOnly = false;
+            }
+            else
+            {
+                richTextBox1.ReadOnly = true;
+            }
+        }
+    }
+}
